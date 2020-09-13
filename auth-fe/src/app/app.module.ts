bfd4d294eee0041b8090da4aa3547938d5af4e80
deepsh10 <dpshah10793@gmail.com>
2020-09-14 00:33:20 +0530

    Completed Sign Up

--- auth-fe/src/app/app.module.ts
+++ auth-fe/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
